b19a62a1269b0a2c27c986bb57097b91bf8c9bac
Kirill <dads@sad>
2020-03-20 11:59:32 +0300

    fix errors handlers

--- src/components/UI/Input/index.js
+++ src/components/UI/Input/index.js
@@ -57,25 +57,6 @@ const Input = (props) => {
 
   const errMsg = errorsHandlerInput(props.error, props.elementType, props.isSignup, props.errorMsg)
 
-  if (props.error && props.error.message === 'INVALID_PASSWORD' && props.elementType === 'password') {
-    errMsg = props.errorMsg
-  }
-  if (props.error && props.isSignup && props.error.message === 'WEAK_PASSWORD : Password should be at least 6 characters' && props.elementType === 'password') {
-    errMsg = props.errorMsg
-  }
-  if (props.error && props.error.message === 'INVALID_EMAIL' && props.elementType === 'text') {
-    errMsg = props.errorMsg
-  }
-  if (props.error && props.error.message === 'EMAIL_NOT_FOUND' && props.elementType === 'text') {
-    errMsg = props.errorMsg
-  }
-  if (props.error && props.isSignup && props.error.message === 'EMAIL_EXISTS' && props.elementType === 'text') {
-    errMsg = props.errorMsg
-  }
-  if (props.error && props.isSignup && props.error.message === 'INVALID_EMAIL' && props.elementType === 'text') {
-    errMsg = props.errorMsg
-  }
-
   return (
     <div className='input-section'>
 
--- src/utils/errorsHandler.js
+++ src/utils/errorsHandler.js
@@ -1,56 +1,56 @@
 export const errorsHandler = (error) => {
-    console.log(error)
-    if (error) {
+  console.log(error)
+  if (error) {
     if (error && error.message === 'INVALID_PASSWORD') {
-      let errorMsg = 'Неверный парол'
+      let errorMsg = 'Неверный пароль'
       return errorMsg
     }
     if (error && error.message === 'INVALID_EMAIL') {
       let errorMsg = 'Ошибка, неверный email. Попробуйте снова'
       return errorMsg
     }
-    
+
     if (error && error.message === 'EMAIL_EXISTS') {
       let errorMsg = 'Ошибка, email уже существует. Попробуйте другой'
       return errorMsg
     }
-    if (error  && error.message === 'EMAIL_NOT_FOUND') {
+    if (error && error.message === 'EMAIL_NOT_FOUND') {
       let errorMsg = 'Ошибка, учетной записи с таким email не существует. Попробуйте другой'
       return errorMsg
     }
-    if (error  && error.message === 'WEAK_PASSWORD : Password should be at least 6 characters') {
+    if (error && error.message === 'WEAK_PASSWORD : Password should be at least 6 characters') {
       let errorMsg = 'Ошибка, ненадежный пароль.Пароль должен состоять минимум из 6 символов'
       return errorMsg
     }
-    }
+  }
 }
 
 export const errorsHandlerInput = (error, type, isSignup, message) => {
-    if (error) {
-       if (error && error.message === 'INVALID_PASSWORD' && type === 'password') {
-        let errMsg = message
-        return errMsg
-      }
-      if (error && isSignup && error.message === 'WEAK_PASSWORD : Password should be at least 6 characters' && type === 'password') {
-        let errMsg = message
-        return errMsg
-      }
-      if (error && error.message === 'INVALID_EMAIL' && type === 'text') {
-        let errMsg = message
-        return errMsg
-      }
-      if (error && error.message === 'EMAIL_NOT_FOUND' && type === 'text') {
-        let errMsg = message
-        return errMsg
-      }
-      if (error && isSignup && error.message === 'EMAIL_EXISTS' && type === 'text') {
-        let errMsg = message
-        return errMsg
-      }
-      if (error && isSignup && error.message === 'INVALID_EMAIL' && type === 'text') {
-        let errMsg = message
-        return errMsg
-      } 
+  if (error) {
+    if (error && error.message === 'INVALID_PASSWORD' && type === 'password') {
+      let errMsg = 'Неверный пароль'
+      return errMsg
     }
+    if (error && error.message === 'INVALID_EMAIL' && type === 'text') {
+      let errMsg = 'Ошибка, неверный email. Попробуйте снова'
+      return errMsg
+    }
+    if (error && error.message === 'EMAIL_NOT_FOUND' && type === 'text') {
+      let errMsg = 'Ошибка, учетной записи с таким email не существует. Попробуйте другой'
+      return errMsg
+    }
+    if (error && isSignup && error.message === 'EMAIL_EXISTS' && type === 'text') {
+      let errMsg = 'Ошибка, email уже существует. Попробуйте другой'
+      return errMsg
+    }
+    if (error && isSignup && error.message === 'INVALID_EMAIL' && type === 'text') {
+      let errMsg = 'Ошибка, неверный email. Попробуйте снова'
+      return errMsg
+    }
+    if (error && isSignup && error.message === 'WEAK_PASSWORD : Password should be at least 6 characters' && type === 'password') {
+      let errMsg = 'Ошибка, ненадежный пароль.Пароль должен состоять минимум из 6 символов'
+      return errMsg
+    }
+  }
 
 }
